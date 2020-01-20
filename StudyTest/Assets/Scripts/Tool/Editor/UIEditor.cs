75405492e86e31985d52a45dba65095b09ee7158
SamStudy9527 <1210168670@qq.com>
2020-01-20 10:03:39 +0800

    tag:导入一些tool类，新建基础材质

---
+++ StudyTest/Assets/Scripts/Tool/Editor/UIEditor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace HelpProject
+{
+    public class UIEditor : MonoBehaviour
+    {
+
+        //将UI下的所有image text里默认的material替换成自己的写的材质球
+        [MenuItem("Tools/替换所有UI的材质球")]
+        public static void ReplaceUIMaterial()
+        {
+            GameObject go = Selection.activeGameObject;
+
+            string path = "Assets/Materials/JayW/WorldUIMaterial.mat";
+            material = AssetDatabase.LoadAssetAtPath<Material>(path);
+            //Debug.Log(material.name);
+            GetChildAndReplaceMaterial(go.transform);
+            AssetDatabase.SaveAssets();
+            Debug.Log("已经将选择的UI的材质球设置为WorldUIMaterial");
+        }
+
+
+        [MenuItem("Tools/恢复所有UI的材质球为空")]
+        public static void ReplaceUIMaterialNull()
+        {
+            GameObject go = Selection.activeGameObject;
+            material = null;
+            GetChildAndReplaceMaterial(go.transform);
+            AssetDatabase.SaveAssets();
+            Debug.Log("已经将选择的UI的材质球设置为空");
+        }
+
+
+
+        static Material material = null;
+        static void GetChildAndReplaceMaterial(Transform t)
+        {
+            for (int i = 0; i < t.childCount; i++)
+            {
+                Undo.RecordObject(t.gameObject, t.gameObject.name);
+                if (t.GetChild(i).GetComponent<Image>()) t.GetChild(i).GetComponent<Image>().material = material;
+                if (t.GetChild(i).GetComponent<Text>()) t.GetChild(i).GetComponent<Text>().material = material;
+                EditorUtility.SetDirty(t.gameObject);
+                GetChildAndReplaceMaterial(t.GetChild(i));
+            }
+        }
+
+        static List<string> NameArray = new List<string>();
+        [MenuItem("Tools/复制名字")]
+        public static void CopyName()
+        {
+            NameArray.Clear();
+            GameObject go = Selection.activeGameObject;
+            NameArray.Add(go.name);
+            Transform[] coms = go.GetComponentsInChildren<Transform>();
+            foreach (Transform item in coms)
+            {
+                NameArray.Add(item.name);
+            }
+        }
+
+        [MenuItem("Tools/粘贴名字")]
+        public static void PasteName()
+        {
+            GameObject go = Selection.activeGameObject;
+
+            go.name = NameArray[0];
+
+            Transform[] coms = go.GetComponentsInChildren<Transform>();
+            for (int i = 0; i < coms.Length; i++)
+            {
+                coms[i].name = NameArray[i + 1];
+            }
+        }
+    }
+}
---
+++ StudyTest/Assets/Scripts/Tool/Tools.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Sam
+{
+    //------------------------【工具方法】--------------------------------------
+    public static class Tools
+    {
+
+        /// <summary>计算向量 A 和向量 B 在轴 axis 确定的平面上投影（分量）的夹角 </summary>
+        /// <param name="dirA">向量 A</param>
+        /// <param name="dirB">向量 B</param>
+        /// <param name="axis">平面的法线向量</param>
+        /// <returns>A、B在平面上的夹角</returns>
+        public static float AngleAroundAxis(Vector3 dirA, Vector3 dirB, Vector3 axis)
+        {
+            // A、B减去在轴上的投影后，剩下的部分是在轴确定的平面上的分量
+            dirA -= Vector3.Project(dirA, axis);
+            dirB -= Vector3.Project(dirB, axis);
+
+            // 计算夹角，返回的角度总是两个向量之间的锐角(也就是说,它们之间2个可能的角度中较小的那个,不大于180度)。 
+            float angle = Vector3.Angle(dirA, dirB);
+
+            // 确定从 A 到 B 是正转还反转
+            return angle * (Vector3.Dot(axis, Vector3.Cross(dirA, dirB)) < 0 ? -1 : 1);
+        }
+
+        /// <summary>
+        /// 计算欧拉角进行合理化处理
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static float CheckAngle(this float Value)
+        {
+            float angle = Value - 180f;
+            return angle > 0 ? angle - 180f : angle + 180f;
+        }
+
+    }
+
+    /// <summary> 这是用于在检视面板只读属性的组件 </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+#endif
+
+    //------------------------【扩展方法】--------------------------------------
+    /// <summary> 扩展方法 </summary>
+    public static class EMethods
+    {
+
+        /// <summary> 省去[RequireComponent]诸多麻烦的、安全地取得游戏对象上组件的方法，一般用在初始化的时候 </summary>
+        public static T GetSafeComponent<T>(this GameObject go) where T : Component
+        {
+            T component = go.GetComponent<T>();
+            if (!component)
+                Debug.LogError("想要查找 " + typeof(T) + " 类型的组件，但是没找到。", go);
+            return component;
+        }
+
+        /// <summary> /// 设置：修改Transform.EulerAngles.y分量的拓展方法 /// </summary>
+        /// <param name="tr"></param>
+        /// <param name="y"></param>
+        public static void SetYOfEulerAngles(this Transform tr, float y)
+        {
+            Vector3 euler = tr.eulerAngles;
+            euler.y = y;
+            tr.eulerAngles = euler;
+        }
+    }
+    //--------------------------------------------------------------------------
+}
